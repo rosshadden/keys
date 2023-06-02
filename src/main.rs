7ce3019c4edb7bb1629449df62e67b7ba1f991dd
Ross Hadden <rosshadden@gmail.com>
2023-06-02 10:41:52 -0400

    feat: command routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None, propagate_version = true)]
+struct Cli {
+	#[command(subcommand)]
+	command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+	Get {},
+	Set { layer: String },
+	Start {},
+	Stop {},
+}
+
+fn start() {
+	print!("start");
+}
+
+fn stop() {
+	print!("stop");
+}
+
+fn get() {
+	print!("get");
+}
+
+fn set(layer: String) {
+	print!("set: {}", layer);
+}
+
 fn main() {
-	println!("Hello, world!");
+	let cli = Cli::parse();
+
+	match cli.command {
+		Commands::Get {} => get(),
+		Commands::Set { layer } => set(layer),
+		Commands::Start {} => start(),
+		Commands::Stop {} => stop(),
+	}
 }
